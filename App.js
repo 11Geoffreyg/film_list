5f6a0246a9aa8c9159020ee72c44a7e251c4103f
Geoffrey <geoffrey.guez@chekk.me>
2021-02-24 11:17:40 +0100

    first app.js modification

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{ flex: 1, backgroundColor: '#ff7500', borderRadius: 10, justifyContent: 'center'}}>
+        <View style={{ height: 50, backgroundColor: 'white', borderRadius: 15}}></View>
+      </View>
+      <View style={{ flex: 5, backgroundColor: 'green'}}></View>
+      <StatusBar style="light" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/
