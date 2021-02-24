65e9bad8bc8d6de0bd7fe37da092eed4254f86f3
Geoffrey <geoffrey.guez@chekk.me>
2021-02-24 13:26:58 +0100

    recovery

--- App.js
+++ App.js
@@ -5,10 +5,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
-      <View style={{ flex: 1, backgroundColor: '#ff7500', borderRadius: 10, justifyContent: 'center'}}>
-        <View style={{ height: 50, backgroundColor: 'white', borderRadius: 15}}></View>
+      <View style={{ flex: 1, backgroundColor: '#ff7500', borderRadius: 10, justifyContent: 'center', padding: 10}}>
+        <View style={{ height: 50, backgroundColor: 'white', borderRadius: 15, flex: 0, justifyContent: 'center', padding: 10}}>
+          <Text style={{ color: 'red'}}>Bonjour</Text>
+        </View>
       </View>
-      <View style={{ flex: 5, backgroundColor: 'green'}}></View>
+      <View style={{ flex: 5, backgroundColor: 'green'}}>
+      </View>
       <StatusBar style="light" />
     </View>
   );
